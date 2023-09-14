d1f820b40d1c66c992131081bf615c0f587034ac
Storm Harris-Studdart <103412904+StormHS@users.noreply.github.com>
2023-09-14 10:11:00 +0200

    Add files via upload
    Saving code here, it will eventually all add up to create the Cookie Consent App once I have worked through all the challenges. Currently won't be working all together

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
-const revealBtn = document.getElementById('reveal-btn')
-const answer = document.getElementById('answer')
-const question = document.getElementById('question')
+const modal = document.getElementById('modal')
+const modalCloseBtn = document.getElementById('modal-close-btn')
+ 
+setTimeout(function(){
+    modal.style.display = 'inline'
+}, 1500)
 
-revealBtn.addEventListener('click', function(){
-    answer.style.display = 'block'
-    question.style.color = '#1434A4'
-    question.style.backgroundColor = '#68e1fd'
-    revealBtn.style.display = 'none'
+modalCloseBtn.addEventListener('click', function(){
+    modal.style.display = 'none'
 })
-
-/*
-Challenge
-1. When the button is clicked and the answer revealed, 
-   change the background color to #68e1fd and text color
-   to #1434A4 just in the "question" div.
-   ⚠️ You will run into a problem with the background 
-   color. See if you can find the solution by googling.
-2. When the answer is revealed, make the button disappear.
-*/
