335216972f48976d2ae10c91707f28b4c0aa6796
Storm Harris-Studdart <103412904+StormHS@users.noreply.github.com>
2023-09-19 09:48:14 +0200

    Add files via upload
    Worked on more challenges in Scrimba

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const modal = document.getElementById('modal')
 const modalCloseBtn = document.getElementById('modal-close-btn')
- 
+
 setTimeout(function(){
     modal.style.display = 'inline'
 }, 1500)
